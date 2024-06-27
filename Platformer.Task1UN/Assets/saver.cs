8d2d702a0e5e29880f640526f35f64d48cdc1c5c
dimkarasev <kard.1234569@gmail.com>
2024-06-27 14:20:04 +1000

    fixed something

--- Platformer.Task1UN/Assets/saver.cs
+++ Platformer.Task1UN/Assets/saver.cs
@@ -4,9 +4,10 @@ public class saver : MonoBehaviour
 {
     [SerializeField] private savedPosition pos;
     [SerializeField] private AudioSource saveSound;
-    private void OnTriggerEnter2D(Collider2D other)
+    [SerializeField] private PlayerControllerPlatformer player;
+    private void FixedUpdate()
     {
-        if (transform.position != pos.position)
+        if (transform.position != pos.position && Vector3.Distance(transform.position, player.transform.position) <2 )
         {
             pos.position = transform.position;
             saveSound.Play();
