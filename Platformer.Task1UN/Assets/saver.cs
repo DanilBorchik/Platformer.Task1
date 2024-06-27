5a3a5c695784f3ffce7bdd10e80782f65e8fe4f7
dimkarasev <kard.1234569@gmail.com>
2024-06-27 15:22:15 +1000

    автоматический поиск игрока чекпоинтами, теперь привязка вручную не требуется

--- Platformer.Task1UN/Assets/saver.cs
+++ Platformer.Task1UN/Assets/saver.cs
@@ -1,10 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class saver : MonoBehaviour
 {
     [SerializeField] private savedPosition pos;
     [SerializeField] private AudioSource saveSound;
     [SerializeField] private PlayerControllerPlatformer player;
+
+    private void Start()
+    {
+        if (player == null)
+        {
+            player = FindObjectOfType<PlayerControllerPlatformer>();
+        }
+    }
+
     private void FixedUpdate()
     {
         if (transform.position != pos.position && Vector3.Distance(transform.position, player.transform.position) <2 )
