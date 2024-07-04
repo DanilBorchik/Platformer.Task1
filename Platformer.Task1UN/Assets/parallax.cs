31e06df75c45f1d4ec9adaff78a56b73c934c88f
dimkarasev <kard.1234569@gmail.com>
2024-07-04 13:40:37 +1000

    parallax!

---
+++ Platformer.Task1UN/Assets/parallax.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Vector2 = UnityEngine.Vector2;
+
+public class parallax : MonoBehaviour
+{
+    private Image _image;
+    private Material _paralMaterial;
+    [SerializeField] private Material parallaxMaterial;
+    [SerializeField] private float currentOffset;
+    [SerializeField] private float offsetMul = 0.01f;
+    void Start()
+    {
+        _image = GetComponent<Image>();
+        _paralMaterial = new Material(parallaxMaterial);
+        _image.material = _paralMaterial;
+    }
+
+
+    void Update()
+    {
+        currentOffset = Camera.main.transform.position.x;
+        _paralMaterial.mainTextureOffset = new Vector2(Camera.main.transform.position.x*offsetMul, 0);
+    }
+}
