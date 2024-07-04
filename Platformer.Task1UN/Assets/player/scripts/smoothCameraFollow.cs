46255e21f189cfef5c0a4855923be93d68356224
dimkarasev <kard.1234569@gmail.com>
2024-07-04 14:25:44 +1000

    теперь камера немного отдаляется при поднятии/опускании

--- Platformer.Task1UN/Assets/player/scripts/smoothCameraFollow.cs
+++ Platformer.Task1UN/Assets/player/scripts/smoothCameraFollow.cs
@@ -6,16 +6,18 @@ public class smoothCameraFollow : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private Vector3 offset;
     [SerializeField] private float sensitivity;
+    public float camerasize;
     void Start()
     {
-        
+        camerasize = Camera.main.orthographicSize;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         transform.position = Vector3.Lerp
-            (transform.position, objectToFollow.transform.position + offset - 
+            (transform.position, objectToFollow.transform.position + offset + 
                   new Vector3(0, Input.GetAxis("Vertical")*sensitivity), speed*Time.fixedDeltaTime);
+        Camera.main.orthographicSize = Mathf.Abs(Input.GetAxis("Vertical"))+camerasize;
     }
 }
